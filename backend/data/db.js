5d711b5855290b8372dacc6240582c980c5958dc
surafel-23 <surafelnega23@gmail.com>
2024-11-21 14:43:57 +0300

    new file:   .gitignore 	modified:   backend/.env 	modified:   backend/data/db.js 	modified:   backend/package.json 	deleted:    kasSysGroup/favicon.ico 	modified:   kasSysGroup/src/app/components/events/events.component.html 	modified:   kasSysGroup/src/app/components/events/events.component.ts 	modified:   kasSysGroup/src/app/components/footer/footer.component.html 	modified:   kasSysGroup/src/app/components/header/header.component.html 	modified:   kasSysGroup/src/app/components/products/products.component.html 	deleted:    kasSysGroup/src/assets/favicon.ico 	renamed:    kasSysGroup/src/assets/pngegg (2).png -> kasSysGroup/src/assets/icons/chatbot-icon.png 	renamed:    kasSysGroup/src/assets/fax.png -> kasSysGroup/src/assets/icons/fax.png 	renamed:    kasSysGroup/src/assets/location.png -> kasSysGroup/src/assets/icons/location.png 	renamed:    kasSysGroup/src/assets/mail.png -> kasSysGroup/src/assets/icons/mail.png 	renamed:    kasSysGroup/src/assets/phone-call.png -> kasSysGroup/src/assets/icons/phone-call.png 	renamed:    kasSysGroup/src/assets/pngegg (1).png -> kasSysGroup/src/assets/icons/pngegg (1).png 	renamed:    kasSysGroup/src/assets/erm 1.jpeg -> kasSysGroup/src/assets/images/erm 1.jpeg 	renamed:    kasSysGroup/src/assets/erm.jpg -> kasSysGroup/src/assets/images/err.jpeg 	renamed:    kasSysGroup/src/assets/kassys_banner.jpg -> kasSysGroup/src/assets/images/kassys_banner.jpg 	renamed:    kasSysGroup/src/assets/kassys_logo1.jpg -> kasSysGroup/src/assets/images/kassys_logo1.jpg 	renamed:    kasSysGroup/src/assets/transformational_success_upward.jpg -> kasSysGroup/src/assets/images/transformational_success.jpg 	renamed:    kasSysGroup/src/assets/images err.jpeg -> kasSysGroup/src/assets/images/welcome.jpg 	deleted:    kasSysGroup/src/favicon.ico

--- backend/data/db.js
+++ backend/data/db.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'kassys'
-
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'kassys',
+    ssl: process.env.NODE_ENV === 'production' ? {
+        rejectUnauthorized: true
+    } : false
 });
 
 connection.connect((err) => {
--- kasSysGroup/src/app/components/events/events.component.ts
+++ kasSysGroup/src/app/components/events/events.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './events.component.html',
   styleUrl: './events.component.css'
 })
