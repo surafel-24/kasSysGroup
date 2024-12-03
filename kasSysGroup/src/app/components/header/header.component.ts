51037addef45be67247f67650346bc513538c58b
surafel-23 <surafelnega23@gmail.com>
2024-12-03 20:43:55 +0300

    modified:   kasSysGroup/src/app/components/header/header.component.css 	modified:   kasSysGroup/src/app/components/header/header.component.html 	modified:   kasSysGroup/src/app/components/header/header.component.ts 	modified:   kasSysGroup/src/styles.css

--- kasSysGroup/src/app/components/header/header.component.ts
+++ kasSysGroup/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,11 +12,27 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
   constructor(private router: Router){}
   isMenuOpen: boolean = false;
+  isScrolled: boolean = false;
+ lastScrollTop: number = 0;
 
    toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  
+  // Handle scroll
+@HostListener('window:scroll', [])
+onWindowScroll() {
+  const st = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Hide menu on scroll down
+  if (st > this.lastScrollTop && this.isMenuOpen) {
+    this.isMenuOpen = false;
+  }
+
+  // Add background on scroll
+  this.isScrolled = st > 50;
+
+  this.lastScrollTop = st <= 0 ? 0 : st;
+}
   
   home(){
     this.router.navigate(['/']);
